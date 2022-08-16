7181a0f93b641184442ff619c617dcf526d37e9e
Jiayi Zhang <jiayi.zhang-5@student.manchester.ac.uk>
2022-08-16 22:27:07 +0100

    add faster solution from discussion

--- fix_names_in_a_table.sql
+++ fix_names_in_a_table.sql
@@ -1,3 +1,8 @@
 SELECT user_id, CONCAT(UPPER(LEFT(name,1)), LOWER(SUBSTR(name, 2, LENGTH(name)))) AS name
 FROM Users
 ORDER BY user_id;
+
+-- faster
+SELECT user_id, CONCAT(UPPER(LEFT(name,1)), LOWER(SUBSTRING(name,2))) as name
+FROM Users
+ORDER BY user_id;
