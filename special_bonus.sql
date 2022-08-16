d98c0556dc3d6fa7941e9e565ebb7908e686477c
Jiayi Zhang <jiayi.zhang-5@student.manchester.ac.uk>
2022-08-16 13:02:10 +0100

    use IF

--- special_bonus.sql
+++ special_bonus.sql
@@ -6,11 +6,17 @@
 -- FROM Employees
 -- ORDER BY employee_id;
 
--- faster
-SELECT employee_id,
-CASE
-    WHEN LEFT(name,1) != 'M' AND MOD(employee_id,2)=1 THEN salary
-    ELSE 0
-END AS bonus
+-- -- faster
+-- SELECT employee_id,
+-- CASE
+--     WHEN LEFT(name,1) != 'M' AND MOD(employee_id,2)=1 THEN salary
+--     ELSE 0
+-- END AS bonus
 
-FROM Employees ORDER BY employee_id;
+-- FROM Employees ORDER BY employee_id;
+
+-- IF
+SELECT employee_id,
+IF (employee_id % 2 != 0 AND name NOT LIKE 'M%', salary , 0) AS bonus
+FROM Employees
+ORDER BY employee_id;
