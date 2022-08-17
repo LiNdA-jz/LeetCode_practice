0eed3a0824dbe63b4fecd71d61962cb7cbc909fe
Jiayi Zhang <jiayi.zhang-5@student.manchester.ac.uk>
2022-08-17 12:06:40 +0100

    did not know offset

---
+++ second_highest_salary.sql
@@ -0,0 +1,35 @@
+SELECT IF(COUNT(a.salary)=2, a.salary, NULL) AS SecondHighestSalary
+FROM
+(SELECT salary
+FROM Employee
+ORDER BY salary DESC
+LIMIT 2) a
+ORDER BY salary ASC
+LIMIT 1;
+
+-- OFFSET
+-- The OFFSET offset clause skips the offset rows before beginning to return the rows.
+SELECT
+    (SELECT DISTINCT
+            Salary
+        FROM
+            Employee
+        ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1) AS SecondHighestSalary
+;
+
+-- IFNULL(expression, alt_value)
+-- alt_value: The value to return if expression is NULL
+-- faster
+SELECT
+    IFNULL(
+      (SELECT DISTINCT Salary
+       FROM Employee
+       ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1),
+    NULL) AS SecondHighestSalary
+
+-- max
+SELECT MAX(Salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee)
