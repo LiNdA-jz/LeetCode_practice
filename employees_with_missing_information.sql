808d71c7580b0317fb0e8663780929e6b1092bcf
Jiayi Zhang <jiayi.zhang-5@student.manchester.ac.uk>
2022-08-17 11:09:38 +0100

    implement union and full join by left and right join

--- employees_with_missing_information.sql
+++ employees_with_missing_information.sql
@@ -7,3 +7,19 @@
 -- ON a.employee_id = b.employee_id) c
 -- WHERE c.name IS NULL OR c.salary IS NULL
 -- ORDER BY employee_id;
+
+-- union & not in
+SELECT employee_id FROM Employees WHERE employee_id NOT IN (SELECT employee_id FROM Salaries)
+UNION 
+SELECT employee_id FROM Salaries WHERE employee_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY 1 ASC
+
+-- full join by left join union right join -> faster
+SELECT T.employee_id
+FROM  
+  (SELECT * FROM Employees LEFT JOIN Salaries USING(employee_id)
+   UNION 
+   SELECT * FROM Employees RIGHT JOIN Salaries USING(employee_id))
+AS T
+WHERE T.salary IS NULL OR T.name IS NULL
+ORDER BY employee_id;
