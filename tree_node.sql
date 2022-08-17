b5fef983fbf79c8998901aac547dbee6a2a38b61
Jiayi Zhang <jiayi.zhang-5@student.manchester.ac.uk>
2022-08-17 11:38:53 +0100

    use union and if

--- tree_node.sql
+++ tree_node.sql
@@ -7,3 +7,52 @@ CASE
 END AS type
 FROM Tree
 ORDER BY id;
+
+-- official: using union
+SELECT
+    id, 'Root' AS Type
+FROM
+    tree
+WHERE
+    p_id IS NULL
+
+UNION
+
+SELECT
+    id, 'Leaf' AS Type
+FROM
+    tree
+WHERE
+    id NOT IN (SELECT DISTINCT
+            p_id
+        FROM
+            tree
+        WHERE
+            p_id IS NOT NULL)
+        AND p_id IS NOT NULL
+
+UNION
+
+SELECT
+    id, 'Inner' AS Type
+FROM
+    tree
+WHERE
+    id IN (SELECT DISTINCT
+            p_id
+        FROM
+            tree
+        WHERE
+            p_id IS NOT NULL)
+        AND p_id IS NOT NULL
+ORDER BY id;
+
+-- if -> slower
+SELECT
+    atree.id,
+    IF(ISNULL(atree.p_id),
+        'Root',
+        IF(atree.id IN (SELECT p_id FROM tree), 'Inner','Leaf')) Type
+FROM
+    tree atree
+ORDER BY atree.id
