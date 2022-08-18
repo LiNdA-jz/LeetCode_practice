f05f3708245ae86fd6cc04b86b55755af9e8558c
Jiayi Zhang <jiayi.zhang-5@student.manchester.ac.uk>
2022-08-18 11:25:06 +0100

    use using

--- combine_two_tables.sql
+++ combine_two_tables.sql
@@ -2,3 +2,7 @@ SELECT a.firstName, ablastName, b.city, b.state
 FROM Person a
 LEFT JOIN Address b
 ON a.personId = b.personId;
+
+-- using
+SELECT FirstName, LastName, City, State
+FROM Person LEFT JOIN Address USING (PersonId)
