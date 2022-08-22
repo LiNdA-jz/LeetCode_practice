2b626b1b5d05d5c8c6e313d9ddc5995a54796718
acse-d5b38199 <linda-specific@outlook.com>
2022-08-22 11:06:55 +0100

    use other methods from ddiscussion

--- user_activity_for_the_past_30_days_I.sql
+++ user_activity_for_the_past_30_days_I.sql
@@ -3,3 +3,21 @@ FROM Activity
 WHERE activity_date > '2019-06-27' AND activity_date <= '2019-07-27'
 AND activity_type IS NOT NULL
 GROUP BY activity_date;
+
+-- use datediff
+SELECT
+     activity_date as day,
+    COUNT(DISTINCT user_id) as active_users
+    
+FROM Activity
+where 
+    datediff('2019-07-27', activity_date) < 30
+        and
+    activity_date <= '2019-07-27'
+GROUP BY activity_date
+
+-- use HAVING
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+GROUP BY activity_date
+HAVING COUNT(DISTINCT user_id)>0 AND activity_date <= '2019-07-27' AND activity_date > subdate('2019-07-27', 30)
