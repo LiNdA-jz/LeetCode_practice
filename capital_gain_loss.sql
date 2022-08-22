8e2f3d471b01c7a603dbf071ef8d6e78ecbbb7f7
acse-d5b38199 <linda-specific@outlook.com>
2022-08-22 14:43:14 +0100

    use other methods from discussion

--- capital_gain_loss.sql
+++ capital_gain_loss.sql
@@ -3,10 +3,26 @@
 SELECT stock_name, SUM(price_signed) AS capital_gain_loss
 FROM
 (SELECT stock_name, operation_day,
- CASE
+CASE
     WHEN operation = 'Sell' THEN price
     ELSE -1*price
     END AS price_signed
 FROM Stocks
 GROUP BY stock_name, operation_day) a
 GROUP BY stock_name;
+
+-- no subquery
+SELECT stock_name, SUM(
+    CASE
+        WHEN operation = 'Buy' THEN -price
+        ELSE price
+    END
+) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name
+
+-- if
+select stock_name
+, sum(if(operation = 'Buy', -1, 1) * price) as capital_gain_loss
+from stocks
+group by stock_name
