8c242ff2fdd5dfd9e8290fdfa89952cd44c19834
acse-d5b38199 <linda-specific@outlook.com>
2022-08-22 11:37:44 +0100

    commit other solutions

--- customer_placing_the_largest_number_of_orders.sql
+++ customer_placing_the_largest_number_of_orders.sql
@@ -4,3 +4,33 @@ FROM
 FROM Orders
 GROUP BY customer_number
 ORDER BY order_count DESC LIMIT 1) a;
+
+-- no subquery
+SELECT
+    customer_number
+FROM
+    orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC
+LIMIT 1
+;
+
+-- subquery + having
+SELECT customer_number
+FROM orders
+GROUP BY customer_number
+HAVING COUNT(order_number) = (
+	SELECT COUNT(order_number) cnt
+	FROM orders
+	GROUP BY customer_number
+	ORDER BY cnt DESC
+	LIMIT 1
+)
+
+-- rank -> much slower
+select customer_number
+from (
+select customer_number, rank() over (order by count(1) desc) rank_val
+from orders
+group by customer_number
+) a where rank_val = 1
