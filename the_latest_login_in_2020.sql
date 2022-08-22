517e9985341f1e0ba1b1eecb8b553a0eb54999be
acse-d5b38199 <linda-specific@outlook.com>
2022-08-22 12:22:06 +0100

    use like

--- the_latest_login_in_2020.sql
+++ the_latest_login_in_2020.sql
@@ -2,3 +2,9 @@ SELECT user_id, MAX(time_stamp) AS last_stamp
 FROM Logins
 WHERE YEAR(time_stamp) = '2020'
 GROUP BY user_id;
+
+-- like
+select user_id, max(time_stamp) 'last_stamp'
+from logins
+where time_stamp like '2020%'
+group by user_id ;
