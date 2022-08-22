ffcc0562fb51cd075dc486ff47793fddfde10dbd
acse-d5b38199 <linda-specific@outlook.com>
2022-08-22 12:09:40 +0100

    add usage of partition by

--- game_play_analysis_I.sql
+++ game_play_analysis_I.sql
@@ -37,6 +37,20 @@ where
 poz = 1;
 
 -- over + partition by
+-- We can use the SQL PARTITION BY clause with the OVER clause to specify the column on which we need to perform aggregation
+-- We get all records in a table using the PARTITION BY clause.
 select distinct player_id, first_value(event_date) 
 over(partition by player_id order by event_date) first_login 
 from Activity;
+
+-- We can use the SQL PARTITION BY clause with ROW_NUMBER() function to have a row number of each row.
+-- SELECT Customercity, 
+--        CustomerName, 
+--        ROW_NUMBER() OVER(PARTITION BY Customercity
+--        ORDER BY OrderAmount DESC) AS "Row Number", 
+--        OrderAmount, 
+--        COUNT(OrderID) OVER(PARTITION BY Customercity) AS CountOfOrders, 
+--        AVG(Orderamount) OVER(PARTITION BY Customercity) AS AvgOrderAmount, 
+--        MIN(OrderAmount) OVER(PARTITION BY Customercity) AS MinOrderAmount, 
+--        SUM(Orderamount) OVER(PARTITION BY Customercity) TotalOrderAmount
+-- FROM [dbo].[Orders];
