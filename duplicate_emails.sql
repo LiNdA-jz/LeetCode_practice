34fb991b844102544a45e9a5c736bd877f01b149
acse-d5b38199 <linda-specific@outlook.com>
2022-08-23 13:32:06 +0100

    commit other approaches

--- duplicate_emails.sql
+++ duplicate_emails.sql
@@ -4,3 +4,24 @@ FROM
 FROM Person
 GROUP BY email
 HAVING COUNT(*)>1) a;
+
+-- without subquery
+select Email
+from Person
+group by Email
+having count(Email) > 1;
+
+-- where
+select Email from
+(
+  select Email, count(Email) as num
+  from Person
+  group by Email
+) as statistic
+where num > 1
+;
+
+-- inner join
+SELECT distinct p1.Email from Person p1
+INNER JOIN Person p2
+ON p1.Email = p2.Email AND p1.Id <> p2.Id;
