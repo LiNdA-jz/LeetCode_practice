406f5e7fd99d5bb89903a10edc485c0b8691a39c
acse-d5b38199 <linda-specific@outlook.com>
2022-08-24 14:47:59 +0100

    optimise solution

--- algorithm/search_insert_position.py
+++ algorithm/search_insert_position.py
@@ -14,3 +14,22 @@ class Solution:
         
 
         return left
+
+        # efficient
+        if(not nums or len(nums) == 0):
+            return 0;
+        
+        left, right = 0, len(nums)-1
+
+        while (left<right):
+            pivot = left + (right - left)//2
+
+            if (nums[pivot]==target):
+                return pivot
+            elif(target<nums[pivot]):
+                right = pivot
+            else:
+                left = pivot + 1
+        
+
+        return left+1 if (target>nums[left]) else left;
