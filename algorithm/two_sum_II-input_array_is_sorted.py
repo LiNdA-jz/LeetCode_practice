3f2f0a65d1f9034851b8352d200eb3f3eeedc426
acse-d5b38199 <linda-specific@outlook.com>
2022-08-26 15:45:47 +0100

    add other methods and complexity

--- algorithm/two_sum_II-input_array_is_sorted.py
+++ algorithm/two_sum_II-input_array_is_sorted.py
@@ -1,5 +1,7 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # Two pointers: O(n) time and O(1) space
+
         l, r = 0, len(numbers)-1
 
         while (l<r):
@@ -9,3 +11,38 @@ class Solution:
                 l += 1
             else:
                 return [l+1,r+1]
+
+        l, r = 0, len(numbers)-1
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s == target:
+                return [l+1, r+1]
+            elif s < target:
+                l += 1
+            else:
+                r -= 1
+
+
+        # dict
+        # Dictionary: O(n) time and O(n) space
+
+        dic = {}
+        for i, num in enumerate(numbers):
+            if target-num in dic:
+                return [dic[target-num]+1, i+1]
+            dic[num] = i
+
+        
+        # binary search
+        # Binary search: O(nlogn) time and O(1) space
+        for i in range(len(numbers)):
+            l, r = i+1, len(numbers)-1
+            tmp = target - numbers[i]
+            while l <= r:
+                mid = l + (r-l)//2
+                if numbers[mid] == tmp:
+                    return [i+1, mid+1]
+                elif numbers[mid] < tmp:
+                    l = mid+1
+                else:
+                    r = mid-1
