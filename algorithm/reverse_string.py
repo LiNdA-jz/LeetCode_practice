582dbecd2e62fc287df7bcb81ca745911d1a3fdf
Jiayi Zhang <lindazhangjiayi@outlook.com>
2022-08-29 14:12:51 +0100

    other approaches

--- algorithm/reverse_string.py
+++ algorithm/reverse_string.py
@@ -15,5 +15,12 @@ class Solution:
             s = s[-1:r] + s[r] + s[l] + s[l-1:0]
 
 
+        # Time complexity is O(n) and additional space is O(1)
+        for i in range(len(s)//2): 
+            s[i], s[-i-1] = s[-i-1], s[i]
 
         
+        
+        # s[:] = s[::-1] is required NOT s = s[::-1] because you have to edit the list inplace.
+        # Under the hood, s[:] = is editing the actual memory bytes s points to, and s = points the variable name s to other bytes in the memory.
+        s[:] = s[::-1]
