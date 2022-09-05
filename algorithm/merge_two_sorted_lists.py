42614dfae6e6350197357b870e2f44d43adf427d
Jiayi Zhang <lindazhangjiayi@outlook.com>
2022-09-05 18:29:48 +0100

    use dummy and merge iteratively

---
+++ algorithm/merge_two_sorted_lists.py
@@ -0,0 +1,57 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # if (len(list1)==0):
+        #     return list2
+        # if (len(list2)==0):
+        #     return list1
+        
+
+        # dummy node, two pointers -> iterative
+        # Time: O(n)
+        # Space: O(1)
+        cur = dummy = ListNode()
+        while list1 and list2:               
+            if list1.val < list2.val:
+                cur.next = list1
+                list1, cur = list1.next, list1
+            else:
+                cur.next = list2
+                list2, cur = list2.next, list2
+                
+        if list1 or list2:
+            cur.next = list1 if list1 else list2
+            
+        return dummy.next
+
+        # recursive
+        if not l1 or not l2:
+            return l1 or l2
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+        # in-place iteratively
+        if None in (l1, l2):
+            return l1 or l2
+        dummy = cur = ListNode(0)
+        dummy.next = l1
+        while l1 and l2:
+            if l1.val < l2.val:
+                l1 = l1.next
+            else:
+                nxt = cur.next
+                cur.next = l2
+                tmp = l2.next
+                l2.next = nxt
+                l2 = tmp
+            cur = cur.next
+        cur.next = l1 or l2
+        return dummy.next
