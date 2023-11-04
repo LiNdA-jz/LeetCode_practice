a05a6750d7599656ba511ba97007c0476cd37dae
Jiayi <lindazhangjiayi@outlook.com>
2023-11-04 21:17:41 +0800

    Practice on 04/11/2023

---
+++ all_topics/Letter_Combinations_of_a_Phone_Number.py
@@ -0,0 +1,90 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        # low performance
+        num_dict = {'2':['a','b','c'],
+                 '3':['d','e','f'],
+                 '4':['g','h','i'],
+                 '5':['j','k','l'],
+                 '6':['m','n','o'],
+                 '7':['p','q','r','s'],
+                 '8':['t','u','v'],
+                 '9':['w','x','y','z']}
+
+        res_ls = []
+        len_digits = len(digits)
+
+        if len_digits == 0:
+            return res_ls
+
+        first = num_dict[digits[0]]
+        if len_digits == 1:
+            return first
+        else:
+            for i in first:
+                for j in self.letterCombinations(digits[1:]):
+                    res_ls.append(i+j)
+
+            return res_ls
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return list()
+
+        phoneMap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def backtrack(index: int):
+            if index == len(digits):
+                combinations.append("".join(combination))
+            else:
+                digit = digits[index]
+                for letter in phoneMap[digit]:
+                    combination.append(letter)
+                    backtrack(index + 1)
+                    combination.pop()
+
+        combination = list()
+        combinations = list()
+        backtrack(0)
+        return combinations
+
+    # 作者：力扣官方题解
+    # 链接：https://leetcode.cn/problems/letter-combinations-of-a-phone-number/
+    # 来源：力扣（LeetCode）
+    # 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: return []
+
+        phone = {'2': ['a', 'b', 'c'],
+                 '3': ['d', 'e', 'f'],
+                 '4': ['g', 'h', 'i'],
+                 '5': ['j', 'k', 'l'],
+                 '6': ['m', 'n', 'o'],
+                 '7': ['p', 'q', 'r', 's'],
+                 '8': ['t', 'u', 'v'],
+                 '9': ['w', 'x', 'y', 'z']}
+
+        def backtrack(conbination, nextdigit):
+            if len(nextdigit) == 0:
+                res.append(conbination)
+            else:
+                for letter in phone[nextdigit[0]]:
+                    backtrack(conbination + letter, nextdigit[1:])
+
+        res = []
+        backtrack('', digits)
+        return res
+
+    # 作者：腐烂的橘子
+    # 链接：https://leetcode.cn/problems/letter-combinations-of-a-phone-number/
+    # 来源：力扣（LeetCode）
+    # 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
