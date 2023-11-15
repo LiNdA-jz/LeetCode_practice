e7dc3dfb12e8fc089b2a53aa8893095757e8ba7b
Jiayi <lindazhangjiayi@outlook.com>
2023-11-15 22:46:08 +0800

    Practice on 15/11/2023

--- all_topics/combinationSum.py
+++ all_topics/combinationSum.py
@@ -17,5 +17,30 @@ class Solution:
         
         dfs(0,[],0)
         return res
-                    
-            
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        if target < min(candidates):
+            return []
+
+        res = []
+        for i in range(len(candidates)):
+            cur_sum = candidates[i]
+            cur_num_count = 1
+
+            while cur_sum <= target:
+                if cur_sum == target:
+                    res.append([candidates[i]] * cur_num_count)
+                    # print(res)
+                    break
+                else:
+                    diff = target - cur_sum
+                    if i != len(candidates) - 1:
+                        sub_comb = self.combinationSum(candidates[i + 1:], diff)
+                        if len(sub_comb) != 0:
+                            for next_comb in sub_comb:
+                                res.append([candidates[i]] * cur_num_count + next_comb)
+                                # print(res)
+                    cur_sum += candidates[i]
+                    cur_num_count += 1
+
+        return res
